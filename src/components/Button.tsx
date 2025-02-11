16a873b8126768296ad60086dbff6a32be20899a
Daniele <dddaaaniii.98@gmail.com>
2025-02-11 17:10:34 +0100

    feat: add cva variants and styling for button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,31 +8,118 @@ interface ButtonProps
   children: ReactNode;
 }
 
-const ButtonVariants = cva('', {
-  variants: {
-    variant: {
-      solid: '',
-      outlined: '',
-      transparent: ''
+const ButtonVariants = cva(
+  [
+    'inline-flex',
+    'gap-2',
+    'items-center',
+    'justify-center',
+    'font-semibold',
+    'leading-none',
+    'tracking-wide',
+    'whitespace-nowrap',
+    'transition-colors',
+    'duration-200',
+    'hover:opacity-70',
+    'focus:outline-none',
+    'disabled:opacity-50',
+    'disabled:cursor-not-allowed'
+  ],
+  {
+    variants: {
+      as: {
+        btn: '',
+        icon: 'px-0'
+      },
+      variant: {
+        default: 'bg-indigo-600 border text-white',
+        outlined: 'bg-white border border-black text-black',
+        transparent: 'bg-transparent border-none text-black'
+      },
+      size: {
+        sm: 'text-sm py-1 px-2',
+        md: 'text-base py-2 px-4',
+        lg: 'text-base py-3 px-6'
+      },
+      rounded: {
+        default: '',
+        full: 'rounded-full'
+      }
     },
-    size: {
-      sm: '',
-      md: '',
-      lg: ''
+    compoundVariants: [
+      {
+        as: 'btn',
+        size: 'sm',
+        rounded: 'default',
+        class: 'rounded-sm'
+      },
+      {
+        as: 'btn',
+        size: 'md',
+        rounded: 'default',
+        class: 'rounded-md'
+      },
+      {
+        as: 'btn',
+        size: 'lg',
+        rounded: 'default',
+        class: 'rounded-lg'
+      },
+      {
+        as: 'icon',
+        size: 'sm',
+        rounded: 'default',
+        class: 'rounded-sm h-8 w-8'
+      },
+      {
+        as: 'icon',
+        size: 'md',
+        rounded: 'default',
+        class: 'rounded-md h-10 w-10'
+      },
+      {
+        as: 'icon',
+        size: 'lg',
+        rounded: 'default',
+        class: 'rounded-lg h-12 w-12'
+      },
+      {
+        as: 'icon',
+        size: 'sm',
+        rounded: 'full',
+        class: 'h-8 w-8'
+      },
+      {
+        as: 'icon',
+        size: 'md',
+        rounded: 'full',
+        class: 'h-10 w-10'
+      },
+      {
+        as: 'icon',
+        size: 'lg',
+        rounded: 'full',
+        class: 'h-12 w-12'
+      }
+    ],
+    defaultVariants: {
+      as: 'btn',
+      variant: 'default',
+      size: 'md',
+      rounded: 'default'
     }
-  },
-  defaultVariants: {
-    variant: 'solid',
-    size: 'md'
   }
-});
+);
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ variant, size, children, ...props }, ref) => {
+  ({ as, variant, size, rounded, className, children, ...props }, ref) => {
     return (
       <button
         ref={ref}
-        className={cn(ButtonVariants({ variant, size }))}
+        className={cn(
+          ButtonVariants({ as, variant, size, rounded }),
+          className
+        )}
         {...props}
       >
         {children}
