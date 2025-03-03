57ad20fa933633fc8ce416f4d83049b5b7db23c8
Daniele <dddaaaniii.98@gmail.com>
2025-03-03 18:42:58 +0100

    build: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vitest/config";
+import peerDependencies from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ exclude: "**/*.test.tsx" })],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setupTests.ts'
-  }
+    environment: "jsdom",
+    setupFiles: "./tests/setupTests.ts",
+  },
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: "./lib/index.ts",
+      name: "ui",
+      fileName: (format) => `ui.${format}.js`,
+      formats: ["es", "cjs", "umd"],
+    },
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+      output: { globals: { react: "React", "react-dom": "ReactDOM" } },
+    },
+  },
 });
