c79476d2a5c6865e2a2fe433fc399638dcf97213
Daniele <dddaaaniii.98@gmail.com>
2025-03-06 15:53:56 +0100

    fix: update tailwind config for correctly loading tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  important: ".ui",
   prefix: "ui-",
   content: ["./lib/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
