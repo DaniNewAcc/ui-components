e76d74a81c4f8b07e1c0d8dad054b4e27b093f34
Daniele <dddaaaniii.98@gmail.com>
2025-03-12 16:22:43 +0100

    chore: add export for card component on index file

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,4 +1,5 @@
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Separator } from './Separator';
-export { default as Text } from './Text';
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Image } from "./Image";
+export { default as Separator } from "./Separator";
+export { default as Text } from "./Text";
