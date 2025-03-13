4ca50e98e37b4a3ca18169cc60d44ff57cc1ec53
Daniele <dddaaaniii.98@gmail.com>
2025-03-13 17:02:53 +0100

    feat: add cva styling for image component with props (border, borderColor, fit, zoom) and compound variant

--- lib/components/Image/Image.tsx
+++ lib/components/Image/Image.tsx
@@ -2,20 +2,52 @@ import { cva, VariantProps } from "class-variance-authority";
 import { ImgHTMLAttributes } from "react";
 import { cn } from "../../utils/cn";
 
-const ImageVariants = cva("ui-block", {
-  variants: {
-    rounded: {
-      default: "",
-      sm: "ui-rounded-sm",
-      md: "ui-rounded-md",
-      lg: "ui-rounded-lg",
-      full: "ui-rounded-full",
+const ImageVariants = cva(
+  "ui-block ui-cursor-pointer ui-transition-all ui-ease-in ui-duration-200",
+  {
+    variants: {
+      border: {
+        default: "",
+        sm: "ui-border",
+        md: "ui-border-2",
+        lg: "ui-border-4",
+      },
+      borderColor: {
+        primary: "ui-border-primary-500",
+        transparent: "ui-border-transparent",
+      },
+      fit: {
+        default: "",
+        contain: "ui-object-contain",
+        cover: "ui-object-cover",
+        none: "ui-object-none",
+        scaleDown: "ui-object-scale-down",
+      },
+      rounded: {
+        default: "",
+        sm: "ui-rounded-sm",
+        md: "ui-rounded-md",
+        lg: "ui-rounded-lg",
+        full: "ui-rounded-full",
+      },
+      zoom: {
+        true: "hover:ui-scale-110",
+      },
+    },
+    compoundVariants: [
+      {
+        border: ["sm", "md", "lg"],
+        borderColor: ["primary", "transparent"],
+        class: "hover:ui-border-primary-800",
+      },
+    ],
+    defaultVariants: {
+      border: "default",
+      fit: "default",
+      rounded: "default",
     },
   },
-  defaultVariants: {
-    rounded: "default",
-  },
-});
+);
 
 interface ImageProps
   extends ImgHTMLAttributes<HTMLImageElement>,
@@ -29,7 +61,11 @@ interface ImageProps
 
 const Image = ({
   ref,
+  border,
+  borderColor,
+  fit,
   rounded,
+  zoom,
   sourceImg,
   altText,
   width,
@@ -45,7 +81,10 @@ const Image = ({
       alt={altText}
       height={height}
       width={width}
-      className={cn(ImageVariants({ rounded }), className)}
+      className={cn(
+        ImageVariants({ border, borderColor, fit, rounded, zoom }),
+        className,
+      )}
       {...props}
     />
   );
