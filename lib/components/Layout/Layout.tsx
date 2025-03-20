19400ff2d56ea1781ce0e32c3ba273f0663d3ffd
Daniele <dddaaaniii.98@gmail.com>
2025-03-20 14:34:56 +0100

    feat: create layout component

---
+++ lib/components/Layout/Layout.tsx
@@ -0,0 +1,27 @@
+import { PolymorphicComponent } from "../../utils/types";
+
+type LayoutProps<C extends React.ElementType> = PolymorphicComponent<
+  C,
+  { as?: C; testId?: string }
+>;
+
+const Layout = <C extends React.ElementType = "div">({
+  as,
+  testId,
+  children,
+  ...props
+}: LayoutProps<C>) => {
+  const Tag = as || "div";
+
+  return (
+    <Tag
+      data-testid={testId}
+      className="ui-flex ui-h-screen ui-w-screen ui-flex-col ui-overflow-x-hidden"
+      {...props}
+    >
+      {children}
+    </Tag>
+  );
+};
+
+export default Layout;
