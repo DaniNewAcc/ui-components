ca0230349212ed51f5babf0a85f571fa83da7aa8
Daniele <dddaaaniii.98@gmail.com>
2025-03-25 14:28:44 +0100

    feat: add type utility for component with ref

--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -1,16 +1,28 @@
 import React from "react";
 
 // allow to dynamically define the native html element for the component
-export type AsProp<C extends React.ElementType> = {
+type AsProp<C extends React.ElementType> = {
   as?: C;
 };
 
 // omit props strictly related to the html element defined so it can be changed without losing html attributes support
-export type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+// helper for passing correctly ref to the component
+export type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>["ref"];
 
 // allow support for html attributes related to the component defined by the AsProp type
+
+// without ref
 export type PolymorphicComponent<
   C extends React.ElementType,
   Props = object,
 > = React.PropsWithChildren<Props & AsProp<C>> &
-  Omit<React.ComponentProps<C>, PropsToOmit<C, Props>>;
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+// with ref
+export type PolymorphicComponentWithRef<
+  C extends React.ElementType,
+  Props = object,
+> = PolymorphicComponent<C, Props> & { ref?: PolymorphicRef<C> };
