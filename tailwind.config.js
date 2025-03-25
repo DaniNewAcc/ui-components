a0914d09fac2083b97277c6aa89c584734d445c4
Daniele <dddaaaniii.98@gmail.com>
2025-03-25 14:21:59 +0100

    feat: add tailwind setting for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   prefix: "ui-",
+  darkMode: "class",
   content: ["./lib/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
