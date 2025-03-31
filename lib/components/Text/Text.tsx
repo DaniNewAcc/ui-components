0c1542e8c2c0989e7d71a886191c70863bbee402
Daniele <ddaaaniii.98@gmail.com>
2025-03-31 14:49:19 +0200

    feat: add basic cva styling for text component

--- lib/components/Text/Text.tsx
+++ lib/components/Text/Text.tsx
@@ -3,8 +3,22 @@ import { PolymorphicComponent } from "@utils/types";
 import { cva, VariantProps } from "class-variance-authority";
 
 const TextVariants = cva("", {
-  variants: {},
-  defaultVariants: {},
+  variants: {
+    variant: {
+      default: "",
+      heading: "ui:scroll-mt-16 ui:tracking-tight",
+      list: "ui:list-disc ui:[&>li]:mt-2",
+    },
+    border: {
+      default: "",
+      bottom: "ui:border-b ui:pb-2",
+      left: "ui:border-l ui:pl-4",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    border: "default",
+  },
 });
 
 type TextProps<C extends React.ElementType> = VariantProps<
@@ -14,13 +28,20 @@ type TextProps<C extends React.ElementType> = VariantProps<
 
 const Text = <C extends React.ElementType = "span">({
   as,
+  border,
+  variant,
+  className,
   testId,
   children,
   ...props
 }: TextProps<C>) => {
   const Tag = as || "span";
   return (
-    <Tag data-testid={testId} className={cn(TextVariants({}))} {...props}>
+    <Tag
+      data-testid={testId}
+      className={cn(TextVariants({ variant, border }), className)}
+      {...props}
+    >
       {children}
     </Tag>
   );
