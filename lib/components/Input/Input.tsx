e143942e836d16557fe4a5d13e7962f14b4ffd7a
Daniele <ddaaaniii.98@gmail.com>
2025-04-01 15:05:02 +0200

    feat: add basic styling for input component

--- lib/components/Input/Input.tsx
+++ lib/components/Input/Input.tsx
@@ -1,18 +1,18 @@
 import { cn } from "@/utils/cn";
-import { cva, VariantProps } from "class-variance-authority";
 import React, { ComponentPropsWithoutRef, forwardRef } from "react";
 
-const InputVariants = cva("", {
-  variants: {},
-  defaultVariants: {},
-});
-
-type InputProps = ComponentPropsWithoutRef<"input"> &
-  VariantProps<typeof InputVariants> & {};
+type InputProps = ComponentPropsWithoutRef<"input">;
 
 const Input = forwardRef<React.ElementRef<"input">, InputProps>(
-  ({ ...props }, ref) => (
-    <Input ref={ref} className={cn(InputVariants({}))} {...props} />
+  ({ className, ...props }, ref) => (
+    <input
+      ref={ref}
+      className={cn(
+        "ui:w-full ui:h-8 ui:flex ui:px-4 ui:py-2 ui:rounded-md ui:border ui:border-primary-600 ui:placeholder-primary-500 ui:transition-all ui:duration-200 ui:outline-hidden ui:focus:placeholder-primary-800 ui:focus:border-primary-900",
+        className,
+      )}
+      {...props}
+    />
   ),
 );
 export default Input;
