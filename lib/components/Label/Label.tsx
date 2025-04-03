25e4c8f9060b7a98a7c3b52336acc0fbcb29b572
Daniele <ddaaaniii.98@gmail.com>
2025-04-03 23:31:18 +0200

    feat: add basic styling for label component

--- lib/components/Label/Label.tsx
+++ lib/components/Label/Label.tsx
@@ -1,11 +1,24 @@
 import { cn } from "@/utils/cn";
-import { ComponentPropsWithoutRef, forwardRef } from "react";
+import { ComponentPropsWithoutRef, forwardRef, ReactNode } from "react";
 
-type LabelProps = ComponentPropsWithoutRef<"label"> & {};
+type LabelProps = ComponentPropsWithoutRef<"label"> & {
+  children?: ReactNode;
+};
 
 const Label = forwardRef<React.ElementRef<"label">, LabelProps>(
-  ({ className, ...props }, ref) => {
-    return <label ref={ref} className={cn("", className)} {...props} />;
+  ({ children, className, ...props }, ref) => {
+    return (
+      <label
+        ref={ref}
+        className={cn(
+          "ui:flex ui:items-center ui:gap-4 ui:font-medium",
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </label>
+    );
   },
 );
 
