6d21580e6c36d588d1274c9c816b9bab367dbc93
Daniele <ddaaaniii.98@gmail.com>
2025-04-12 11:55:43 +0200

    test: add unit testing for dots loader type

--- tests/components/Loader.test.tsx
+++ tests/components/Loader.test.tsx
@@ -2,14 +2,25 @@ import { Loader } from "@/components";
 import { render, screen } from "@testing-library/react";
 
 describe("Loader", () => {
-  it("should render", () => {
+  it("should render spinner loader", () => {
     render(
       <Loader loaderType={"spinner"} testId="loader">
         Loading...
       </Loader>,
     );
     const loader = screen.getByTestId("loader");
-    expect(loader.hasAttribute("loaderType"));
+    expect(loader.hasAttribute("spinner"));
+    expect(loader).toBeInTheDocument();
+  });
+
+  it("should render dots loader", () => {
+    render(
+      <Loader loaderType={"dots"} testId="loader">
+        Loading...
+      </Loader>,
+    );
+    const loader = screen.getByTestId("loader");
+    expect(loader.hasAttribute("dots"));
     expect(loader).toBeInTheDocument();
   });
 });
