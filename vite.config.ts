29ab0c3e63b6a6b09e3ffef5fea3f04ad79c9901
Daniele <ddaaaniii.98@gmail.com>
2025-04-22 16:03:27 +0200

    chore: update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ import dts from "vite-plugin-dts";
 import { defineConfig } from "vitest/config";
 import peerDependencies from "./package.json";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), dts({ exclude: "**/*.test.tsx" })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({
+      include: ["lib"],
+      exclude: ["**/*.test.tsx", "**/__tests__/**, tests/**"],
+      insertTypesEntry: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./lib"),
@@ -24,6 +31,7 @@ export default defineConfig({
     setupFiles: "./tests/setupTests.ts",
   },
   build: {
+    minify: true,
     copyPublicDir: false,
     lib: {
       entry: "./lib/index.ts",
@@ -32,6 +40,7 @@ export default defineConfig({
       formats: ["es", "cjs", "umd"],
     },
     rollupOptions: {
+      treeshake: true,
       external: Object.keys(peerDependencies),
       output: { globals: { react: "React", "react-dom": "ReactDOM" } },
     },
