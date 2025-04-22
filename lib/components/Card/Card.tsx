db397686b6860fbb943fc0c87af3a52d7db0c688
Daniele <ddaaaniii.98@gmail.com>
2025-04-22 17:00:34 +0200

    feat: add onclick prop and shift as variant for hovereffects

--- lib/components/Card/Card.tsx
+++ lib/components/Card/Card.tsx
@@ -1,82 +1,70 @@
-import { cn } from "@utils/cn";
-import { cva, VariantProps } from "class-variance-authority";
-import React, { ComponentPropsWithRef, forwardRef, ReactNode } from "react";
+import { cn } from '@utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import React, { ComponentPropsWithRef, forwardRef, ReactNode } from 'react';
 
 const CardVariants = cva(
-  "ui:relative ui:flex ui:cursor-pointer ui:flex-col ui:gap-4 ui:overflow-hidden ui:bg-primary-50 ui:transition-all ui:duration-200 ui:ease-out",
+  'ui:relative ui:flex ui:cursor-pointer ui:flex-col ui:gap-4 ui:overflow-hidden ui:bg-primary-50 ui:transition-all ui:duration-200 ui:ease-out',
   {
     variants: {
       border: {
-        default: "ui:border",
-        outlined: "ui:border-2 ui:border-primary-500",
-        topLeft:
-          "ui:border-t-2 ui:border-l-2 ui:border-t-primary-50 ui:border-l-primary-50",
+        default: 'ui:border',
+        outlined: 'ui:border-2 ui:border-primary-500',
+        topLeft: 'ui:border-t-2 ui:border-l-2 ui:border-t-primary-50 ui:border-l-primary-50',
       },
       hoverEffect: {
-        default: "",
-        zoom: "ui:hover:scale-110",
+        default: '',
+        zoom: 'ui:hover:scale-110',
+        shift: 'ui:hover:translate-x-2 ui:hover:translate-y-2',
       },
       padding: {
-        sm: "ui:p-2",
-        md: "ui:p-4",
-        lg: "ui:p-6",
+        sm: 'ui:p-2',
+        md: 'ui:p-4',
+        lg: 'ui:p-6',
       },
       rounded: {
-        sm: "ui:rounded-sm",
-        md: "ui:rounded-md",
-        lg: "ui:rounded-lg",
+        sm: 'ui:rounded-sm',
+        md: 'ui:rounded-md',
+        lg: 'ui:rounded-lg',
       },
       shadow: {
-        sm: "ui:shadow-sm",
-        md: "ui:shadow-md",
-        lg: "ui:shadow-lg",
+        sm: 'ui:shadow-sm',
+        md: 'ui:shadow-md',
+        lg: 'ui:shadow-lg',
       },
     },
     defaultVariants: {
-      border: "default",
-      hoverEffect: "default",
-      padding: "md",
-      rounded: "md",
-      shadow: "md",
+      border: 'default',
+      hoverEffect: 'default',
+      padding: 'md',
+      rounded: 'md',
+      shadow: 'md',
     },
-  },
+  }
 );
 
-type CardProps = ComponentPropsWithRef<"article"> &
+type CardProps = ComponentPropsWithRef<'article'> &
   VariantProps<typeof CardVariants> & {
     testId?: string;
+    onClick?: React.MouseEventHandler;
     children: ReactNode;
   };
 
-const Card = forwardRef<React.ElementRef<"article">, CardProps>(
+const Card = forwardRef<React.ElementRef<'article'>, CardProps>(
   (
-    {
-      border,
-      hoverEffect,
-      padding,
-      rounded,
-      shadow,
-      testId,
-      className,
-      children,
-      ...props
-    },
-    ref,
+    { border, hoverEffect, padding, rounded, shadow, testId, className, children, ...props },
+    ref
   ) => {
     return (
       <article
         ref={ref}
         data-testid={testId}
-        className={cn(
-          CardVariants({ border, hoverEffect, padding, rounded, shadow }),
-          className,
-        )}
+        className={cn(CardVariants({ border, hoverEffect, padding, rounded, shadow }), className)}
         {...props}
       >
         {children}
       </article>
     );
-  },
+  }
 );
 
 export default Card;
