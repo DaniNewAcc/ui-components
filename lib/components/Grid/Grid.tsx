29c7101850780f3a279383a6593ec1bbf969c6e9
Daniele <ddaaaniii.98@gmail.com>
2025-04-24 01:02:52 +0200

    feat: add display variant for grid component

--- lib/components/Grid/Grid.tsx
+++ lib/components/Grid/Grid.tsx
@@ -1,82 +1,87 @@
-import { cn } from "@utils/cn";
-import { PolymorphicComponent } from "@utils/types";
-import { cva, VariantProps } from "class-variance-authority";
-import { ElementType } from "react";
+import { cn } from '@utils/cn';
+import { PolymorphicComponent } from '@utils/types';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ElementType } from 'react';
 
-const GridVariants = cva("ui:grid", {
+const GridVariants = cva('', {
   variants: {
+    display: {
+      grid: 'ui:grid',
+      inlineGrid: 'ui:inline-grid',
+      none: 'ui:hidden',
+    },
     gap: {
-      default: "",
-      xs: "ui:gap-1",
-      sm: "ui:gap-2",
-      md: "ui:gap-4",
-      lg: "ui:gap-8",
-      xl: "ui:gap-12",
-      xxl: "ui:gap-16",
+      default: '',
+      xs: 'ui:gap-1',
+      sm: 'ui:gap-2',
+      md: 'ui:gap-4',
+      lg: 'ui:gap-8',
+      xl: 'ui:gap-12',
+      xxl: 'ui:gap-16',
     },
     colGap: {
-      default: "",
-      xs: "ui:gap-x-1",
-      sm: "ui:gap-x-2",
-      md: "ui:gap-x-4",
-      lg: "ui:gap-x-8",
-      xl: "ui:gap-x-12",
-      xxl: "ui:gap-x-16",
+      default: '',
+      xs: 'ui:gap-x-1',
+      sm: 'ui:gap-x-2',
+      md: 'ui:gap-x-4',
+      lg: 'ui:gap-x-8',
+      xl: 'ui:gap-x-12',
+      xxl: 'ui:gap-x-16',
     },
     rowGap: {
-      default: "",
-      xs: "ui:gap-y-1",
-      sm: "ui:gap-y-2",
-      md: "ui:gap-y-4",
-      lg: "ui:gap-y-8",
-      xl: "ui:gap-y-12",
-      xxl: "ui:gap-y-16",
+      default: '',
+      xs: 'ui:gap-y-1',
+      sm: 'ui:gap-y-2',
+      md: 'ui:gap-y-4',
+      lg: 'ui:gap-y-8',
+      xl: 'ui:gap-y-12',
+      xxl: 'ui:gap-y-16',
     },
     gridCol: {
-      default: "",
-      2: "ui:grid-cols-2",
-      3: "ui:grid-cols-3",
-      4: "ui:grid-cols-4",
-      5: "ui:grid-cols-5",
-      6: "ui:grid-cols-6",
-      7: "ui:grid-cols-7",
-      8: "ui:grid-cols-8",
-      9: "ui:grid-cols-9",
-      10: "ui:grid-cols-10",
-      11: "ui:grid-cols-11",
-      12: "ui:grid-cols-12",
+      default: '',
+      2: 'ui:grid-cols-2',
+      3: 'ui:grid-cols-3',
+      4: 'ui:grid-cols-4',
+      5: 'ui:grid-cols-5',
+      6: 'ui:grid-cols-6',
+      7: 'ui:grid-cols-7',
+      8: 'ui:grid-cols-8',
+      9: 'ui:grid-cols-9',
+      10: 'ui:grid-cols-10',
+      11: 'ui:grid-cols-11',
+      12: 'ui:grid-cols-12',
     },
     gridRow: {
-      default: "",
-      2: "ui:grid-rows-2",
-      3: "ui:grid-rows-3",
-      4: "ui:grid-rows-4",
-      5: "ui:grid-rows-5",
-      6: "ui:grid-rows-6",
-      7: "ui:grid-rows-7",
-      8: "ui:grid-rows-8",
-      9: "ui:grid-rows-9",
-      10: "ui:grid-rows-10",
-      11: "ui:grid-rows-11",
-      12: "ui:grid-rows-12",
+      default: '',
+      2: 'ui:grid-rows-2',
+      3: 'ui:grid-rows-3',
+      4: 'ui:grid-rows-4',
+      5: 'ui:grid-rows-5',
+      6: 'ui:grid-rows-6',
+      7: 'ui:grid-rows-7',
+      8: 'ui:grid-rows-8',
+      9: 'ui:grid-rows-9',
+      10: 'ui:grid-rows-10',
+      11: 'ui:grid-rows-11',
+      12: 'ui:grid-rows-12',
     },
   },
   defaultVariants: {
-    gap: "default",
-    colGap: "default",
-    rowGap: "default",
-    gridCol: "default",
-    gridRow: "default",
+    display: 'grid',
+    gap: 'default',
+    colGap: 'default',
+    rowGap: 'default',
+    gridCol: 'default',
+    gridRow: 'default',
   },
 });
 
-type GridProps<C extends React.ElementType> = VariantProps<
-  typeof GridVariants
-> &
+type GridProps<C extends React.ElementType> = VariantProps<typeof GridVariants> &
   PolymorphicComponent<C, { as?: C; testId?: string }>;
 
-const Grid = <C extends ElementType = "div">({
+const Grid = <C extends ElementType = 'div'>({
   as,
+  display,
   gap,
   colGap,
   rowGap,
@@ -87,14 +92,11 @@ const Grid = <C extends ElementType = "div">({
   children,
   ...props
 }: GridProps<C>) => {
-  const Tag = as || "div";
+  const Tag = as || 'div';
   return (
     <Tag
       data-testid={testId}
-      className={cn(
-        GridVariants({ gap, colGap, rowGap, gridCol, gridRow }),
-        className,
-      )}
+      className={cn(GridVariants({ display, gap, colGap, rowGap, gridCol, gridRow }), className)}
       {...props}
     >
       {children}
