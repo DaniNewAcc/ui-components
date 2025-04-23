67ec33c5043e10e5b8813391adba1932534fcea3
Daniele <ddaaaniii.98@gmail.com>
2025-04-24 01:08:16 +0200

    feat: add error styling variant for input component

--- lib/components/Input/Input.tsx
+++ lib/components/Input/Input.tsx
@@ -1,18 +1,28 @@
-import { cn } from "@/utils/cn";
-import React, { ComponentPropsWithoutRef, forwardRef } from "react";
+import { cn } from '@/utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import React, { ComponentPropsWithoutRef, forwardRef } from 'react';
 
-type InputProps = ComponentPropsWithoutRef<"input">;
+const InputVariants = cva(
+  'ui:flex ui:h-8 ui:w-full ui:rounded-md ui:border ui:px-3 ui:py-2 ui:text-base ui:shadow-sm ui:transition-all ui:duration-200 ui:outline-none ui:focus:ring-2 ui:disabled:pointer-events-none ui:disabled:cursor-not-allowed ui:disabled:opacity-50',
+  {
+    variants: {
+      error: {
+        true: 'ui:border-red-600 ui:text-red-600 ui:placeholder-red-500 ui:focus:border-red-800',
+        false:
+          'ui:border-primary-600 ui:text-primary-500 ui:placeholder-primary-500 ui:focus:border-primary-900',
+      },
+    },
+    defaultVariants: {
+      error: false,
+    },
+  }
+);
 
-const Input = forwardRef<React.ElementRef<"input">, InputProps>(
-  ({ className, ...props }, ref) => (
-    <input
-      ref={ref}
-      className={cn(
-        "ui:flex ui:h-8 ui:w-full ui:rounded-md ui:border ui:border-primary-600 ui:px-4 ui:py-2 ui:text-base ui:text-primary-500 ui:placeholder-primary-500 ui:shadow-sm ui:transition-all ui:duration-200 ui:outline-none ui:focus:border-primary-900 ui:focus:ring ui:disabled:pointer-events-none ui:disabled:cursor-not-allowed ui:disabled:opacity-50",
-        className,
-      )}
-      {...props}
-    />
-  ),
+type InputProps = ComponentPropsWithoutRef<'input'> & VariantProps<typeof InputVariants>;
+
+const Input = forwardRef<React.ElementRef<'input'>, InputProps>(
+  ({ error, className, ...props }, ref) => (
+    <input ref={ref} className={cn(InputVariants({ error }), className)} {...props} />
+  )
 );
 export default Input;
