cf895a393f295567c683f786e0cf9405c62c79f1
Daniele <ddaaaniii.98@gmail.com>
2025-04-24 01:10:52 +0200

    feat: add overridable className for layout component

--- lib/components/Layout/Layout.tsx
+++ lib/components/Layout/Layout.tsx
@@ -1,26 +1,27 @@
-import { cn } from "@/utils/cn";
-import { PolymorphicComponent } from "@utils/types";
+import { cn } from '@/utils/cn';
+import { PolymorphicComponent } from '@utils/types';
 
 type LayoutProps<C extends React.ElementType> = PolymorphicComponent<
   C,
   { as?: C; testId?: string }
 >;
 
-const Layout = <C extends React.ElementType = "div">({
+const Layout = <C extends React.ElementType = 'div'>({
   as,
   testId,
   className,
   children,
   ...props
 }: LayoutProps<C>) => {
-  const Tag = as || "div";
+  const Tag = as || 'div';
 
   return (
     <Tag
       data-testid={testId}
+      role={as === 'main' ? 'main' : undefined}
       className={cn(
-        "ui:flex ui:h-screen ui:w-screen ui:flex-col ui:items-center ui:justify-center ui:overflow-x-hidden",
-        className,
+        'ui:flex ui:h-screen ui:w-screen ui:flex-col ui:items-center ui:justify-center ui:overflow-x-hidden',
+        className
       )}
       {...props}
     >
