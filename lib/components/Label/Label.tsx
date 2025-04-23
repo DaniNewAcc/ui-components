5a98341ccec68c44556abdf21d79de8454ed9c70
Daniele <ddaaaniii.98@gmail.com>
2025-04-24 01:09:54 +0200

    feat: add overridable className and htmlfor prop for label component

--- lib/components/Label/Label.tsx
+++ lib/components/Label/Label.tsx
@@ -1,27 +1,25 @@
-import { cn } from "@/utils/cn";
-import { ComponentPropsWithoutRef, forwardRef, ReactNode } from "react";
+import { cn } from '@/utils/cn';
+import { ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react';
 
-type LabelProps = ComponentPropsWithoutRef<"label"> & {
+type LabelProps = ComponentPropsWithoutRef<'label'> & {
+  htmlFor?: string;
   testId?: string;
   children?: ReactNode;
 };
 
-const Label = forwardRef<React.ElementRef<"label">, LabelProps>(
+const Label = forwardRef<React.ElementRef<'label'>, LabelProps>(
   ({ testId, children, className, ...props }, ref) => {
     return (
       <label
         data-testid={testId}
         ref={ref}
-        className={cn(
-          "ui:flex ui:items-center ui:gap-4 ui:font-medium",
-          className,
-        )}
+        className={cn('ui:flex ui:items-center ui:gap-4 ui:font-medium', className)}
         {...props}
       >
         {children}
       </label>
     );
-  },
+  }
 );
 
 export default Label;
