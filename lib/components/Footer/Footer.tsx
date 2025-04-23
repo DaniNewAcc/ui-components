d1dba4e754236ac8829df075c7325814dfe90264
Daniele <ddaaaniii.98@gmail.com>
2025-04-24 01:01:14 +0200

    chore: add testId prop to footer component

--- lib/components/Footer/Footer.tsx
+++ lib/components/Footer/Footer.tsx
@@ -1,53 +1,61 @@
-import { cn } from "@utils/cn";
-import { cva, VariantProps } from "class-variance-authority";
-import { ComponentProps, ReactNode } from "react";
+import { cn } from '@utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ComponentProps, ReactNode } from 'react';
 
-const FooterVariants = cva(
-  "ui:flex ui:w-full ui:items-center ui:justify-center ui:px-4",
-  {
-    variants: {
-      bgColor: {
-        default: "",
-        50: "ui:bg-primary-50",
-        100: "ui:bg-primary-100",
-        200: "ui:bg-primary-200",
-        300: "ui:bg-primary-300",
-        400: "ui:bg-primary-400",
-        500: "ui:bg-primary-500",
-        600: "ui:bg-primary-600",
-        700: "ui:bg-primary-700",
-        800: "ui:bg-primary-800",
-        900: "ui:bg-primary-900",
-      },
-      gap: {
-        default: "",
-        sm: "ui:gap-2",
-        md: "ui:gap-4",
-        lg: "ui:gap-8",
-      },
-      height: {
-        default: "",
-        sm: "ui:h-16",
-        md: "ui:h-20",
-        lg: "ui:h-24",
-      },
+const FooterVariants = cva('ui:flex ui:w-full ui:items-center ui:justify-center', {
+  variants: {
+    bgColor: {
+      default: '',
+      50: 'ui:bg-primary-50',
+      100: 'ui:bg-primary-100',
+      200: 'ui:bg-primary-200',
+      300: 'ui:bg-primary-300',
+      400: 'ui:bg-primary-400',
+      500: 'ui:bg-primary-500',
+      600: 'ui:bg-primary-600',
+      700: 'ui:bg-primary-700',
+      800: 'ui:bg-primary-800',
+      900: 'ui:bg-primary-900',
     },
-    defaultVariants: {
-      bgColor: "default",
-      gap: "default",
-      height: "default",
+    padding: {
+      default: '',
+      sm: 'ui:px-2',
+      md: 'ui:px-4',
+      lg: 'ui:px-8',
+    },
+    gap: {
+      default: '',
+      sm: 'ui:gap-2',
+      md: 'ui:gap-4',
+      lg: 'ui:gap-8',
+    },
+    height: {
+      default: '',
+      sm: 'ui:h-16',
+      md: 'ui:h-20',
+      lg: 'ui:h-24',
     },
   },
-);
+  defaultVariants: {
+    bgColor: 'default',
+    padding: 'default',
+    gap: 'default',
+    height: 'default',
+  },
+});
 
-type FooterProps = ComponentProps<"footer"> &
+type FooterProps = ComponentProps<'footer'> &
   VariantProps<typeof FooterVariants> & {
+    testId?: string;
     children: ReactNode;
   };
 
-const Footer = ({ bgColor, gap, height, className, children }: FooterProps) => {
+const Footer = ({ bgColor, padding, gap, height, className, testId, children }: FooterProps) => {
   return (
-    <footer className={cn(FooterVariants({ bgColor, gap, height }), className)}>
+    <footer
+      data-testid={testId}
+      className={cn(FooterVariants({ bgColor, padding, gap, height }), className)}
+    >
       {children}
     </footer>
   );
