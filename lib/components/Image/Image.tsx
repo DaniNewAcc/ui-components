b17b8501a9805ff1ab5d43ecfb59f1cde44a3c6e
Daniele <ddaaaniii.98@gmail.com>
2025-04-24 01:06:56 +0200

    feat: add testId and sizes prop, add fallback for width and height and default value for loading and altText prop for image component

--- lib/components/Image/Image.tsx
+++ lib/components/Image/Image.tsx
@@ -1,66 +1,68 @@
-import { cn } from "@utils/cn";
-import { cva, VariantProps } from "class-variance-authority";
-import { ComponentPropsWithoutRef, forwardRef } from "react";
+import { cn } from '@utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
 const ImageVariants = cva(
-  "ui:block ui:cursor-pointer ui:transition-all ui:duration-200 ui:ease-in",
+  'ui:block ui:cursor-pointer ui:transition-all ui:duration-200 ui:ease-in',
   {
     variants: {
       border: {
-        default: "",
-        sm: "ui:border",
-        md: "ui:border-2",
-        lg: "ui:border-4",
+        default: '',
+        sm: 'ui:border',
+        md: 'ui:border-2',
+        lg: 'ui:border-4',
       },
       borderColor: {
-        primary: "ui:border-primary-500",
-        transparent: "ui:border-transparent",
+        primary: 'ui:border-primary-500',
+        transparent: 'ui:border-transparent',
       },
       fit: {
-        default: "",
-        contain: "ui:object-contain",
-        cover: "ui:object-cover",
-        none: "ui:object-none",
-        scaleDown: "ui:object-scale-down",
+        default: '',
+        contain: 'ui:object-contain',
+        cover: 'ui:object-cover',
+        none: 'ui:object-none',
+        scaleDown: 'ui:object-scale-down',
       },
       rounded: {
-        default: "",
-        sm: "ui:rounded-sm",
-        md: "ui:rounded-md",
-        lg: "ui:rounded-lg",
-        full: "ui:rounded-full",
+        default: '',
+        sm: 'ui:rounded-sm',
+        md: 'ui:rounded-md',
+        lg: 'ui:rounded-lg',
+        full: 'ui:rounded-full',
       },
       zoom: {
-        true: "ui:hover:scale-110",
+        true: 'ui:hover:scale-110',
       },
     },
     compoundVariants: [
       {
-        border: ["sm", "md", "lg"],
-        borderColor: ["primary", "transparent"],
-        class: "ui:hover:border-primary-800",
+        border: ['sm', 'md', 'lg'],
+        borderColor: ['primary', 'transparent'],
+        class: 'ui:hover:border-primary-800',
       },
     ],
     defaultVariants: {
-      border: "default",
-      fit: "default",
-      rounded: "default",
+      border: 'default',
+      fit: 'default',
+      rounded: 'default',
     },
-  },
+  }
 );
 
-type ImageProps = ComponentPropsWithoutRef<"img"> &
+type ImageProps = ComponentPropsWithoutRef<'img'> &
   VariantProps<typeof ImageVariants> & {
     width?: string | number;
     height?: string | number;
-    loading?: "lazy" | "eager";
+    loading?: 'lazy' | 'eager';
     src?: string;
     srcSet?: string;
     altText?: string;
     sizes?: string;
+    zoom?: boolean;
+    testId?: string;
   };
 
-const Image = forwardRef<React.ElementRef<"img">, ImageProps>(
+const Image = forwardRef<React.ElementRef<'img'>, ImageProps>(
   (
     {
       border,
@@ -68,20 +70,24 @@ const Image = forwardRef<React.ElementRef<"img">, ImageProps>(
       fit,
       rounded,
       zoom,
-      loading = "lazy",
+      loading = 'lazy',
       src,
       srcSet,
       sizes,
-      altText = "Image",
+      altText = 'Image',
       width,
       height,
       className,
+      testId,
       ...props
     },
-    ref,
+    ref
   ) => {
+    width = width || '100%';
+    height = height || 'auto';
     return (
       <img
+        data-testid={testId}
         ref={ref}
         loading={loading}
         src={src}
@@ -90,14 +96,11 @@ const Image = forwardRef<React.ElementRef<"img">, ImageProps>(
         sizes={sizes}
         height={height}
         width={width}
-        className={cn(
-          ImageVariants({ border, borderColor, fit, rounded, zoom }),
-          className,
-        )}
+        className={cn(ImageVariants({ border, borderColor, fit, rounded, zoom }), className)}
         {...props}
       />
     );
-  },
+  }
 );
 
 export default Image;
