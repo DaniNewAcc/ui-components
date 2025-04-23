969e44ebdb4b3423e0a427e5e135af46ed8b9309
Daniele <ddaaaniii.98@gmail.com>
2025-04-24 01:25:41 +0200

    chore: add aria-hidden for improved accessibility on separator component

--- lib/components/Separator/Separator.tsx
+++ lib/components/Separator/Separator.tsx
@@ -1,39 +1,35 @@
-import { cn } from "@utils/cn";
-import { PolymorphicComponent } from "@utils/types";
-import { cva, VariantProps } from "class-variance-authority";
+import { cn } from '@utils/cn';
+import { PolymorphicComponent } from '@utils/types';
+import { cva, VariantProps } from 'class-variance-authority';
 
-const SeparatorVariants = cva("ui:bg-gray-400", {
+const SeparatorVariants = cva('ui:bg-gray-400', {
   variants: {
     orientation: {
-      horizontal: "ui:h-[1px] ui:w-full",
-      vertical: "ui:h-full ui:w-[1px]",
+      horizontal: 'ui:h-[1px] ui:w-full',
+      vertical: 'ui:h-full ui:w-[1px]',
     },
   },
   defaultVariants: {
-    orientation: "horizontal",
+    orientation: 'horizontal',
   },
 });
 
-type SeparatorProps<C extends React.ElementType> = VariantProps<
-  typeof SeparatorVariants
-> &
+type SeparatorProps<C extends React.ElementType> = VariantProps<typeof SeparatorVariants> &
   PolymorphicComponent<C, { as?: C; testId?: string }>;
 
-const Separator = <C extends React.ElementType = "hr">({
+const Separator = <C extends React.ElementType = 'hr'>({
   testId,
   as,
   orientation,
   className,
   ...props
 }: SeparatorProps<C>) => {
-  let Tag = as || "hr";
-  if (orientation === "vertical") {
-    Tag = "div";
-  }
+  let Tag = as ?? (orientation === 'vertical' ? 'hr' : 'div');
   return (
     <Tag
       data-testid={testId}
       role="separator"
+      aria-hidden="true"
       className={cn(SeparatorVariants({ orientation }), className)}
       {...props}
     />
