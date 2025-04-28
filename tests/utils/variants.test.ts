6ddc3ac5f3cc4fe6911205228ce90e7441057e97
Daniele <ddaaaniii.98@gmail.com>
2025-04-28 16:42:25 +0200

    test: add tests for exporting correctly variants from utils

---
+++ tests/utils/variants.test.ts
@@ -0,0 +1,55 @@
+import * as variants from '@/utils/variants';
+
+describe('Variants File Exports', () => {
+  it('should export ButtonVariants', () => {
+    expect(variants.ButtonVariants).toBeDefined();
+  });
+
+  it('should export CardVariants', () => {
+    expect(variants.CardVariants).toBeDefined();
+  });
+
+  it('should export FlexVariants', () => {
+    expect(variants.FlexVariants).toBeDefined();
+  });
+
+  it('should export FooterVariants', () => {
+    expect(variants.FooterVariants).toBeDefined();
+  });
+
+  it('should export GridVariants', () => {
+    expect(variants.GridVariants).toBeDefined();
+  });
+
+  it('should export HeaderVariants', () => {
+    expect(variants.HeaderVariants).toBeDefined();
+  });
+
+  it('should export ImageVariants', () => {
+    expect(variants.ImageVariants).toBeDefined();
+  });
+
+  it('should export InputVariants', () => {
+    expect(variants.InputVariants).toBeDefined();
+  });
+
+  it('should export LoaderVariants', () => {
+    expect(variants.LoaderVariants).toBeDefined();
+  });
+
+  it('should export SeparatorVariants', () => {
+    expect(variants.SeparatorVariants).toBeDefined();
+  });
+
+  it('should export TabsVariants', () => {
+    expect(variants.TabsVariants).toBeDefined();
+  });
+
+  it('should export TabsListVariants', () => {
+    expect(variants.TabsListVariants).toBeDefined();
+  });
+
+  it('should export TextVariants', () => {
+    expect(variants.TextVariants).toBeDefined();
+  });
+});
