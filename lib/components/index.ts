9831fd9277f76edb65a83339ec0278c865556d9a
Daniele <ddaaaniii.98@gmail.com>
2025-05-14 15:44:55 +0200

    refactor: re-export animate component for cleaner import path

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -3,18 +3,20 @@ export {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@components/Accordion";
-export { default as Button } from "@components/Button";
-export { default as Card } from "@components/Card";
-export { default as Flex } from "@components/Flex";
-export { default as Footer } from "@components/Footer";
-export { default as Grid } from "@components/Grid";
-export { default as Header } from "@components/Header";
-export { default as Image } from "@components/Image";
-export { default as Input } from "@components/Input";
-export { default as Label } from "@components/Label";
-export { default as Layout } from "@components/Layout";
-export { default as Loader } from "@components/Loader";
-export { default as Separator } from "@components/Separator";
-export { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/Tabs";
-export { default as Text } from "@components/Text";
+} from '@components/Accordion';
+export { default as Animate } from '@components/Animate';
+export { default as Button } from '@components/Button';
+export { default as Card } from '@components/Card';
+export { default as Flex } from '@components/Flex';
+export { default as Footer } from '@components/Footer';
+export { default as Grid } from '@components/Grid';
+export { default as Header } from '@components/Header';
+export { default as Image } from '@components/Image';
+export { default as Input } from '@components/Input';
+export { default as Label } from '@components/Label';
+export { default as Layout } from '@components/Layout';
+export { default as Loader } from '@components/Loader';
+export { Select, SelectDropdown, SelectOption, SelectTrigger } from '@components/Select';
+export { default as Separator } from '@components/Separator';
+export { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/Tabs';
+export { default as Text } from '@components/Text';
