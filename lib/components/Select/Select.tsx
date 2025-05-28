efe6d81048381b62fcefac987829fd250921c618
Daniele <ddaaaniii.98@gmail.com>
2025-05-28 21:15:55 +0200

    feat(Select): improve UI and overall logic component
    - Introduced keyboard navigation
    
    - Refined styling for trigger, dropdown and option
    
    - Added ARIA attributes for better accessibility
    
    - Memoized context value for better performance
    
    - Added forwardRef to trigger

--- lib/components/Select/Select.tsx
+++ lib/components/Select/Select.tsx
@@ -1,7 +1,20 @@
 import { useClickOutside } from '@/hooks/useClickOutside';
+import { useMergedRefs } from '@/hooks/useMergedRefs';
+import useRovingFocus from '@/hooks/useRovingFocus';
 import { useSyncAnimation } from '@/hooks/useSyncAnimation';
 import { cn } from '@/utils/cn';
-import { ComponentProps, createContext, ReactNode, useCallback, useContext, useState } from 'react';
+import React, {
+  ComponentProps,
+  createContext,
+  forwardRef,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import Animate from '../Animate';
 import { AnimateProps } from '../Animate/Animate';
 
@@ -27,6 +40,13 @@ type SelectContextProps = {
   valueKey: string;
   labelKey: string;
   isDropdownOpen: boolean;
+  focusedIndex: number | string | null;
+  triggerRef: React.RefObject<HTMLButtonElement>;
+  moveFocus: (direction: 'next' | 'previous') => void;
+  moveToStart: () => void;
+  moveToEnd: () => void;
+  setFocusedIndex: React.Dispatch<React.SetStateAction<number | string | null>>;
+  setFocusRef: (SetFocusRefProps: { index: number | string; element: HTMLElement | null }) => void;
   setIsDropdownOpen: React.Dispatch<React.SetStateAction<boolean>>;
   setActiveOption: React.Dispatch<React.SetStateAction<ActiveOption>>;
   handleDropdown: () => void;
@@ -47,12 +67,23 @@ const Select = ({
   children,
   ...props
 }: SelectProps) => {
+  const {
+    focusedIndex,
+    registeredCount,
+    setFocusedIndex,
+    moveFocus,
+    moveToStart,
+    moveToEnd,
+    setFocusRef,
+    clearFocusRefs,
+  } = useRovingFocus(null, loop);
   const [activeOption, setActiveOption] = useState<ActiveOption>(defaultValue);
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
 
-  const handleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+  const handleDropdown = useCallback(() => {
+    setIsDropdownOpen(prev => !prev);
+  }, []);
 
   const handleReset = useCallback((e: React.MouseEvent<HTMLSpanElement>) => {
     e.stopPropagation();
@@ -68,22 +99,63 @@ const Select = ({
       } else {
         setIsDropdownOpen(false);
       }
+      if (triggerRef?.current) {
+        triggerRef.current.focus();
+      }
     },
     [activeOption]
   );
 
-  const contextValue = {
-    options,
-    activeOption,
-    isDropdownOpen,
-    setIsDropdownOpen,
-    setActiveOption,
-    handleDropdown,
-    handleOptions,
-    handleReset,
-    valueKey,
-    labelKey,
-  };
+  const contextValue = useMemo(
+    () => ({
+      options,
+      activeOption,
+      isDropdownOpen,
+      valueKey,
+      labelKey,
+      focusedIndex,
+      triggerRef,
+      setFocusedIndex,
+      moveFocus,
+      moveToStart,
+      moveToEnd,
+      setFocusRef,
+      setIsDropdownOpen,
+      setActiveOption,
+      handleDropdown,
+      handleOptions,
+      handleReset,
+    }),
+    [
+      options,
+      activeOption,
+      isDropdownOpen,
+      valueKey,
+      labelKey,
+      focusedIndex,
+      setFocusedIndex,
+      moveFocus,
+      moveToStart,
+      moveToEnd,
+      setFocusRef,
+      setIsDropdownOpen,
+      setActiveOption,
+      handleDropdown,
+      handleOptions,
+      handleReset,
+    ]
+  );
+
+  useEffect(() => {
+    if (isDropdownOpen && registeredCount === options.length) {
+      requestAnimationFrame(() => {
+        moveToStart();
+      });
+    } else if (!isDropdownOpen) {
+      clearFocusRefs();
+      setFocusedIndex(null);
+    }
+  }, [isDropdownOpen, registeredCount, options.length]);
 
   const ref = useClickOutside<HTMLDivElement>(() => setIsDropdownOpen(false), isDropdownOpen);
   return (
@@ -113,36 +185,75 @@ type SelectTriggerProps = ComponentProps<'button'> & {
   children: ReactNode;
 };
 
-const SelectTrigger = ({
-  placeholderText = 'Select an option...',
-  className,
-  children,
-  testId,
-  ...props
-}: SelectTriggerProps) => {
-  const { activeOption, options, handleReset, handleDropdown, valueKey, labelKey, isDropdownOpen } =
-    useSelectContext();
+const SelectTrigger = forwardRef<HTMLButtonElement, SelectTriggerProps>(
+  (
+    { placeholderText = 'Select an option...', className, children, testId, ...props },
+    externalRef
+  ) => {
+    const {
+      activeOption,
+      options,
+      focusedIndex,
+      handleReset,
+      handleDropdown,
+      triggerRef,
+      valueKey,
+      labelKey,
+      isDropdownOpen,
+      setIsDropdownOpen,
+      moveToStart,
+    } = useSelectContext();
 
-  const selectedOption = options.find(opt => opt[valueKey] === activeOption);
-  return (
-    <button
-      data-testid={testId}
-      className={cn(
-        'ui:flex ui:w-full ui:cursor-pointer ui:justify-between ui:gap-4 ui:rounded-md ui:border-2 ui:px-4 ui:py-1 ui:focus:border-primary-500 ui:focus:outline-none',
-        { 'ui:rounded-b-none ui:border-primary-500': isDropdownOpen },
-        className
-      )}
-      onClick={handleDropdown}
-      {...props}
-    >
-      <span>{activeOption ? selectedOption?.[labelKey] : placeholderText}</span>
-      <div className="ui:flex ui:items-end ui:gap-2">
-        {activeOption ? <span onClick={e => handleReset(e)}>Clear</span> : null}
-        <span>{children}</span>
-      </div>
-    </button>
-  );
-};
+    const mergedRefs = useMergedRefs(triggerRef, externalRef);
+
+    const handleKeyDown = useCallback(
+      (e: React.KeyboardEvent) => {
+        switch (e.key) {
+          case 'ArrowDown':
+          case 'ArrowUp':
+          case 'Enter':
+          case ' ':
+            e.preventDefault();
+            if (!isDropdownOpen) {
+              setIsDropdownOpen(true);
+              moveToStart();
+            }
+            break;
+          default:
+            break;
+        }
+      },
+      [moveToStart]
+    );
+
+    const selectedOption = options.find(opt => opt[valueKey] === activeOption);
+    return (
+      <button
+        ref={mergedRefs}
+        data-testid={testId}
+        aria-haspopup="listbox"
+        aria-expanded={isDropdownOpen}
+        aria-controls="dropdown"
+        aria-activedescendant={focusedIndex !== null ? `option-${focusedIndex}` : undefined}
+        className={cn(
+          'ui:flex ui:w-full ui:cursor-pointer ui:justify-between ui:gap-4 ui:rounded-md ui:border-2 ui:px-4 ui:py-1 ui:focus:outline-none ui:focus-visible:border-primary-500',
+          { 'ui:rounded-b-none ui:border-primary-500': isDropdownOpen },
+          className
+        )}
+        role="combobox"
+        onKeyDown={handleKeyDown}
+        onClick={handleDropdown}
+        {...props}
+      >
+        <span>{activeOption ? selectedOption?.[labelKey] : placeholderText}</span>
+        <div className="ui:flex ui:items-end ui:gap-2">
+          {activeOption ? <span onClick={e => handleReset(e)}>Clear</span> : null}
+          <span>{children}</span>
+        </div>
+      </button>
+    );
+  }
+);
 
 // ------------ Dropdown component
 type SelectDropdownProps = ComponentProps<'ul'> & {
@@ -158,8 +269,41 @@ const SelectDropdown = ({
   children,
   ...props
 }: SelectDropdownProps) => {
-  const { isDropdownOpen } = useSelectContext();
+  const { isDropdownOpen, triggerRef, focusedIndex, setIsDropdownOpen, handleOptions, moveFocus } =
+    useSelectContext();
   const duration = animateProps?.duration ?? 300;
+
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent) => {
+      switch (e.key) {
+        case 'Tab':
+          e.preventDefault();
+          break;
+        case 'ArrowDown':
+          e.preventDefault();
+          moveFocus('next');
+          break;
+        case 'ArrowUp':
+          e.preventDefault();
+          moveFocus('previous');
+          break;
+        case 'Enter':
+        case ' ':
+          e.preventDefault();
+          if (focusedIndex !== null) {
+            handleOptions(focusedIndex);
+          }
+          break;
+        case 'Escape':
+          setIsDropdownOpen(false);
+          if (triggerRef?.current) {
+            triggerRef.current.focus();
+          }
+          break;
+      }
+    },
+    [moveFocus, handleOptions, focusedIndex]
+  );
 
   const { ref, shouldRender, maxHeight } = useSyncAnimation<HTMLUListElement>({
     isOpen: isDropdownOpen,
@@ -172,8 +316,9 @@ const SelectDropdown = ({
         <ul
           ref={ref}
           data-testid={testId}
+          id="dropdown"
           className={cn(
-            'ui:mt-[-2px] ui:flex ui:flex-col ui:rounded-md ui:border-2 ui:px-4 ui:shadow-md',
+            'ui:mt-[-2px] ui:flex ui:flex-col ui:rounded-md ui:border-2 ui:shadow-md',
             { 'ui:rounded-t-none ui:border-primary-500': isDropdownOpen },
             className
           )}
@@ -183,6 +328,9 @@ const SelectDropdown = ({
             opacity: isDropdownOpen ? 1 : 0,
             visibility: isDropdownOpen ? 'visible' : 'hidden',
           }}
+          role="listbox"
+          tabIndex={-1}
+          onKeyDown={handleKeyDown}
           {...props}
         >
           {children}
@@ -200,12 +348,27 @@ type SelectOptionProps = ComponentProps<'li'> & {
 };
 
 const SelectOption = ({ value, className, children, testId, ...props }: SelectOptionProps) => {
-  const { handleOptions } = useSelectContext();
+  const { isDropdownOpen, focusedIndex, handleOptions, setFocusRef } = useSelectContext();
+  const optionRef = useRef<HTMLLIElement | null>(null);
+  const isFocused = focusedIndex === value;
+
+  useEffect(() => {
+    setFocusRef({ index: value, element: optionRef.current });
+  }, [setFocusRef, value]);
 
   return (
     <li
+      ref={optionRef}
       data-testid={testId}
-      className={cn('ui:cursor-pointer ui:first:pt-1 ui:last:pb-2', className)}
+      aria-selected={isFocused ? 'true' : 'false'}
+      id={`option-${value}`}
+      className={cn(
+        { 'ui:bg-primary-600 ui:text-white': isFocused },
+        'ui:cursor-pointer ui:px-4 ui:pt-1 ui:pb-2 ui:last:rounded-b-md ui:focus:outline-none',
+        className
+      )}
+      role="option"
+      tabIndex={isDropdownOpen && isFocused ? 0 : -1}
       onClick={() => handleOptions(value)}
       {...props}
     >
