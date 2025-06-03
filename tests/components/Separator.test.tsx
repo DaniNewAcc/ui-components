394247d24a23606e43d9d219f9254e55a6a185bc
Daniele <ddaaaniii.98@gmail.com>
2025-06-03 23:57:40 +0200

    test(Separator): update unit test

--- tests/components/Separator.test.tsx
+++ tests/components/Separator.test.tsx
@@ -2,23 +2,30 @@ import { Separator } from '@/components';
 import { render, screen } from '@testing-library/react';
 
 describe('Separator', () => {
-  it('should render', () => {
+  it('should render with default element when no props provided', () => {
     render(<Separator />);
-
     const separator = screen.getByRole('separator', { hidden: true });
-    expect(separator.hasAttribute('as'));
+    expect(separator.tagName).toBe('HR');
     expect(separator).toBeInTheDocument();
+    expect(separator).not.toHaveAttribute('as');
   });
-  it('should render based on as prop', () => {
-    render(<Separator as="section" orientation="vertical" />);
 
+  it('should render element based on explicit `as` prop', () => {
+    render(<Separator as="section" orientation="vertical" />);
     const separator = screen.getByRole('separator', { hidden: true });
     expect(separator.tagName).toBe('SECTION');
     expect(separator).toBeInTheDocument();
   });
-  it('should render based on default as prop value', () => {
-    render(<Separator orientation={'horizontal'} />);
 
+  it('should render div when orientation is vertical and no `as` prop is provided', () => {
+    render(<Separator orientation="vertical" />);
+    const separator = screen.getByRole('separator', { hidden: true });
+    expect(separator.tagName).toBe('DIV');
+    expect(separator).toBeInTheDocument();
+  });
+
+  it('should render hr when orientation is horizontal and no `as` prop is provided', () => {
+    render(<Separator orientation="horizontal" />);
     const separator = screen.getByRole('separator', { hidden: true });
     expect(separator.tagName).toBe('HR');
     expect(separator).toBeInTheDocument();
