79927989435417f8f6e60fb9a7147f70cf60f265
Daniele <ddaaaniii.98@gmail.com>
2025-06-09 02:16:11 +0200

    test(Layout): add more unit tests

--- tests/components/Layout.test.tsx
+++ tests/components/Layout.test.tsx
@@ -8,6 +8,7 @@ describe('Layout', () => {
       const layout = screen.getByTestId('layout');
       expect(layout).toBeInTheDocument();
       expect(layout).not.toHaveAttribute('role');
+      expect(layout.className).toMatch('ui:overflow-auto');
     });
 
     it('should render correctly with main role', () => {
@@ -15,6 +16,31 @@ describe('Layout', () => {
       const layout = screen.getByTestId('layout');
       expect(layout).toBeInTheDocument();
       expect(layout).toHaveAttribute('role', 'main');
+    });
+
+    it('should render correctly with isCentered prop', () => {
+      render(<Layout testId="layout" isCentered></Layout>);
+      const layout = screen.getByTestId('layout');
+      expect(layout).toBeInTheDocument();
+      expect(layout).toHaveClass('ui:items-center', 'ui:justify-center');
+      expect(layout).not.toHaveClass('ui:items-start', 'ui:justify-start');
+    });
+
+    it('should render correctly with overflowHidden prop', () => {
+      render(<Layout testId="layout" overflowHidden></Layout>);
+      const layout = screen.getByTestId('layout');
+      expect(layout).toBeInTheDocument();
+      expect(layout.className).toMatch('ui:overflow-hidden');
+      expect(layout.className).not.toMatch('ui:overflow-auto');
+    });
+
+    it('should render correctly with isCentered and overflowHidden set to false', () => {
+      render(<Layout testId="layout" isCentered={false} overflowHidden={false} />);
+      const layout = screen.getByTestId('layout');
+
+      expect(layout).toHaveClass('ui:items-start', 'ui:justify-start');
+      expect(layout).toHaveClass('ui:overflow-auto');
+      expect(layout).not.toHaveClass('ui:items-center', 'ui:justify-center', 'ui:overflow-hidden');
     });
   });
 });
