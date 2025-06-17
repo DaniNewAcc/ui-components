5edcda131b0f3df7aa9aa9b982a0f1c83ff3963d
Daniele <ddaaaniii.98@gmail.com>
2025-06-17 17:31:09 +0200

    feat(Modal): improve component structure and accessibility
    - Added more compound components
    
    - Added ARIA roles for better accessibility

--- lib/components/Modal/Modal.tsx
+++ lib/components/Modal/Modal.tsx
@@ -3,14 +3,15 @@ import useScrollLock from '@/hooks/useScrollLock';
 import { cn } from '@/utils/cn';
 import { ComponentProps, createContext, ReactNode, useCallback, useContext } from 'react';
 import { createPortal } from 'react-dom';
+import Text from '../Text';
 
 type ModalProps = {
   containerId?: string;
   closeOnClickOutside?: boolean;
   testId?: string;
   isOpen: boolean;
-  onClose: () => void;
   children: ReactNode;
+  onClose: () => void;
 };
 
 type ModalContextProps = {
@@ -27,8 +28,8 @@ const Modal = ({
   testId,
   closeOnClickOutside = false,
   isOpen,
-  onClose,
   children,
+  onClose,
 }: ModalProps) => {
   const contextValue = {
     containerId,
@@ -57,12 +58,13 @@ function useModalContext() {
   return context;
 }
 
+// ------------ Portal component
+
 type ModalPortalProps = ComponentProps<'div'> & {
   testId?: string;
-  children: ReactNode;
 };
 
-const ModalPortal = ({ className, testId, children }: ModalPortalProps) => {
+const ModalPortal = ({ className, testId, children, ...props }: ModalPortalProps) => {
   const { containerId, isOpen } = useModalContext();
   const container = usePortal(containerId);
   if (!container) return null;
@@ -70,11 +72,16 @@ const ModalPortal = ({ className, testId, children }: ModalPortalProps) => {
   return createPortal(
     <div
       data-testid={testId}
+      aria-modal="true"
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+      role="dialog"
       className={cn(
         'ui:fixed ui:inset-0 ui:z-50 ui:flex ui:items-center ui:justify-center',
         !isOpen && 'ui:pointer-events-none ui:opacity-0',
         className
       )}
+      {...props}
     >
       {children}
     </div>,
@@ -84,6 +91,8 @@ const ModalPortal = ({ className, testId, children }: ModalPortalProps) => {
 
 ModalPortal.displayName = 'ModalPortal';
 Modal.Portal = ModalPortal;
+
+// ------------ Overlay component
 
 type ModalOverlayProps = ComponentProps<'div'> & {};
 
@@ -109,19 +118,22 @@ const ModalOverlay = ({ className, ...props }: ModalOverlayProps) => {
 ModalOverlay.displayName = 'ModalOverlay';
 Modal.Overlay = ModalOverlay;
 
+// ------------ Content component
+
 type ModalContentProps = ComponentProps<'div'> & {
   testId?: string;
-  children: ReactNode;
 };
 
-const ModalContent = ({ className, testId, children }: ModalContentProps) => {
+const ModalContent = ({ className, testId, children, ...props }: ModalContentProps) => {
   return (
     <div
       data-testid={testId}
+      role="document"
       className={cn(
         'ui:z-50 ui:w-full ui:max-w-lg ui:rounded ui:bg-white ui:p-6 ui:shadow-lg',
         className
       )}
+      {...props}
     >
       {children}
     </div>
@@ -130,5 +142,62 @@ const ModalContent = ({ className, testId, children }: ModalContentProps) => {
 
 ModalContent.displayName = 'ModalContent';
 Modal.Content = ModalContent;
+
+// ------------ Title component
+
+type ModalTitleProps = ComponentProps<'h2'> & {
+  testId?: string;
+};
+
+const ModalTitle = ({ className, testId, children, ...props }: ModalTitleProps) => {
+  return (
+    <Text as="h2" testId={testId} className={cn('', className)} {...props}>
+      {children}
+    </Text>
+  );
+};
+
+ModalTitle.displayName = 'ModalTitle';
+Modal.Title = ModalTitle;
+
+// ------------ Description component
+
+type ModalDescriptionProps = ComponentProps<'p'> & {
+  testId?: string;
+};
+
+const ModalDescription = ({ className, testId, children, ...props }: ModalDescriptionProps) => {
+  return (
+    <Text as="p" testId={testId} className={cn('', className)} {...props}>
+      {children}
+    </Text>
+  );
+};
+
+ModalDescription.displayName = 'ModalDescription';
+Modal.Description = ModalDescription;
+
+// ------------ Footer component
+
+type ModalFooterProps = ComponentProps<'div'> & {
+  testId?: string;
+};
+
+const ModalFooter = ({ className, children, testId, ...props }: ModalFooterProps) => {
+  return (
+    <div
+      data-testid={testId}
+      className={cn('ui:flex ui:justify-end ui:gap-4', className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+ModalFooter.displayName = 'ModalFooter';
+Modal.Footer = ModalFooter;
+
+// ------------ Close component
 
 export default Modal;
