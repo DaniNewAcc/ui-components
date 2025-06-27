745271add6cac01dc2e7544b67fcde6d93b7a331
Daniele <ddaaaniii.98@gmail.com>
2025-06-27 15:41:37 +0200

    test(Text): add unit tests for edge cases based on level prop and heading variant

--- tests/components/Text.test.tsx
+++ tests/components/Text.test.tsx
@@ -1,23 +1,45 @@
-import { Text } from "@/components";
-import { render, screen } from "@testing-library/react";
+import { Text } from '@/components';
+import { render, screen } from '@testing-library/react';
 
-describe("Text", () => {
-  it("should render", () => {
+describe('Text', () => {
+  it('should render', () => {
     render(<Text testId="text">Content</Text>);
 
-    const text = screen.getByTestId("text");
-    expect(text.hasAttribute("as"));
+    const text = screen.getByTestId('text');
+    expect(text.hasAttribute('as'));
     expect(text).toBeInTheDocument();
   });
-  it("should render correctly as a span element", () => {
+  it('should render correctly as a span element', () => {
     render(
-      <Text as={"span"} testId="text">
+      <Text as={'span'} testId="text">
         Content
-      </Text>,
+      </Text>
     );
 
-    const text = screen.getByTestId("text");
-    expect(text.tagName).toBe("SPAN");
+    const text = screen.getByTestId('text');
+    expect(text.tagName).toBe('SPAN');
+    expect(text).toBeInTheDocument();
+  });
+  it('should render correctly as a h1 element', () => {
+    render(
+      <Text variant={'heading'} testId="text">
+        Content
+      </Text>
+    );
+
+    const text = screen.getByTestId('text');
+    expect(text.tagName).toBe('H1');
+    expect(text).toBeInTheDocument();
+  });
+  it('should render correctly as a heading element based on level value', () => {
+    render(
+      <Text variant={'heading'} testId="text" level={3}>
+        Content
+      </Text>
+    );
+
+    const text = screen.getByTestId('text');
+    expect(text.tagName).toBe('H3');
     expect(text).toBeInTheDocument();
   });
 });
