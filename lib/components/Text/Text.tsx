fb2681559cb4a7aa30cec9380e632059551edca9
Daniele <ddaaaniii.98@gmail.com>
2025-06-27 18:36:31 +0200

    feat(Text): improve overall component logic
    - Introduced level prop for handling better heading variant
    
    - Added displayName for improved DevTools experience

--- lib/components/Text/Text.tsx
+++ lib/components/Text/Text.tsx
@@ -4,29 +4,30 @@ import { PolymorphicComponent } from '@utils/types';
 import { VariantProps } from 'class-variance-authority';
 
 type TextProps<C extends React.ElementType> = VariantProps<typeof TextVariants> &
-  PolymorphicComponent<C, { as?: C; testId?: string }>;
+  PolymorphicComponent<C, { as?: C; testId?: string; level?: 1 | 2 | 3 | 4 | 5 | 6 }>;
 
 const Text = <C extends React.ElementType = 'span'>({
   as,
-  border,
   variant,
-  truncate,
-  clamp,
+  level,
   className,
-  testId,
   children,
+  testId,
   ...props
 }: TextProps<C>) => {
-  const Tag = as || 'span';
+  let Tag: React.ElementType = as || 'span';
+
+  if (!as && variant === 'heading') {
+    Tag = level ? `h${level}` : 'h1';
+  }
+
   return (
-    <Tag
-      data-testid={testId}
-      className={cn(TextVariants({ variant, border, truncate, clamp }), className)}
-      {...props}
-    >
+    <Tag data-testid={testId} className={cn(TextVariants({ variant }), className)} {...props}>
       {children}
     </Tag>
   );
 };
+
+Text.displayName = 'Text';
 
 export default Text;
