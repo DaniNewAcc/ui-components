89be8986d8c193dd48f3da9d794bb36b506a307c
Daniele <ddaaaniii.98@gmail.com>
2025-06-28 01:22:01 +0200

    test(Loader): update unit tests

--- tests/components/Loader.test.tsx
+++ tests/components/Loader.test.tsx
@@ -1,26 +1,18 @@
-import { Loader } from "@/components";
-import { render, screen } from "@testing-library/react";
+import { Loader } from '@/components';
+import { render, screen } from '@testing-library/react';
 
-describe("Loader", () => {
-  it("should render spinner loader", () => {
-    render(
-      <Loader loaderType={"spinner"} testId="loader">
-        Loading...
-      </Loader>,
-    );
-    const loader = screen.getByTestId("loader");
-    expect(loader.hasAttribute("spinner"));
+describe('Loader', () => {
+  it('should render spinner loader', () => {
+    render(<Loader loaderType={'spinner'}>Loading...</Loader>);
+    const loader = screen.getByTestId('loader');
+    expect(loader.hasAttribute('spinner'));
     expect(loader).toBeInTheDocument();
   });
 
-  it("should render dots loader", () => {
-    render(
-      <Loader loaderType={"dots"} testId="loader">
-        Loading...
-      </Loader>,
-    );
-    const loader = screen.getByTestId("loader");
-    expect(loader.hasAttribute("dots"));
+  it('should render dots loader', () => {
+    render(<Loader loaderType={'dots'}>Loading...</Loader>);
+    const loader = screen.getByTestId('loader');
+    expect(loader.hasAttribute('dots'));
     expect(loader).toBeInTheDocument();
   });
 });
