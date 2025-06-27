6bcc6338f4192040043cac80345d60ab68c2396f
Daniele <ddaaaniii.98@gmail.com>
2025-06-27 21:53:08 +0200

    test(Input): add new unit tests

--- tests/components/Input.test.tsx
+++ tests/components/Input.test.tsx
@@ -1,10 +1,16 @@
-import { Input } from "@/components";
-import { render, screen } from "@testing-library/react";
+import { Input } from '@/components';
+import { render, screen } from '@testing-library/react';
 
-describe("Input", () => {
-  it("should render", () => {
+describe('Input', () => {
+  it('should render', () => {
     render(<Input></Input>);
-    const input = screen.getByRole("textbox");
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
+  });
+  it('should render with error message and aria-invalid', () => {
+    render(<Input error></Input>);
+    const input = screen.getByRole('textbox');
+    expect(input).toHaveAttribute('aria-invalid', 'true');
     expect(input).toBeInTheDocument();
   });
 });
