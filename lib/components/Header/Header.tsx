29725b0e58404665bb38300da40098fe22c5ae76
Daniele <ddaaaniii.98@gmail.com>
2025-06-29 19:43:15 +0200

    chore(Header): add default value for testId prop and displayName

--- lib/components/Header/Header.tsx
+++ lib/components/Header/Header.tsx
@@ -1,19 +1,26 @@
 import { HeaderVariants } from '@/utils/variants';
 import { cn } from '@utils/cn';
 import { VariantProps } from 'class-variance-authority';
-import { ComponentProps, ReactNode } from 'react';
+import { ComponentProps, forwardRef, ReactNode } from 'react';
 
 type HeaderProps = ComponentProps<'header'> &
   VariantProps<typeof HeaderVariants> & {
+    testId?: string;
     children: ReactNode;
   };
 
-const Header = ({ bgColor, padding, gap, height, className, children, ...props }: HeaderProps) => {
-  return (
-    <header className={cn(HeaderVariants({ bgColor, padding, gap, height }), className)} {...props}>
+const Header = forwardRef<HTMLElement, HeaderProps>(
+  ({ bgColor, padding, gap, height, className, testId = 'header', children, ...props }, ref) => (
+    <header
+      ref={ref}
+      className={cn(HeaderVariants({ bgColor, padding, gap, height }), className)}
+      {...props}
+    >
       {children}
     </header>
-  );
-};
+  )
+);
+
+Header.displayName = 'Header';
 
 export default Header;
