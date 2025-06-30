f222adf9d119547fa95d756c739cfe1dcd34f697
Daniele <ddaaaniii.98@gmail.com>
2025-06-30 12:42:59 +0200

    feat(Footer): add forwardRef support and displayName

--- lib/components/Footer/Footer.tsx
+++ lib/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import { FooterVariants } from '@/utils/variants';
 import { cn } from '@utils/cn';
 import { VariantProps } from 'class-variance-authority';
-import { ComponentProps, ReactNode } from 'react';
+import { ComponentProps, forwardRef, ReactNode } from 'react';
 
 type FooterProps = ComponentProps<'footer'> &
   VariantProps<typeof FooterVariants> & {
@@ -9,15 +9,21 @@ type FooterProps = ComponentProps<'footer'> &
     children: ReactNode;
   };
 
-const Footer = ({ bgColor, padding, gap, height, className, testId, children }: FooterProps) => {
-  return (
-    <footer
-      data-testid={testId}
-      className={cn(FooterVariants({ bgColor, padding, gap, height }), className)}
-    >
-      {children}
-    </footer>
-  );
-};
+const Footer = forwardRef<HTMLElement, FooterProps>(
+  ({ bgColor, padding, gap, height, className, testId = 'footer', children, ...props }, ref) => {
+    return (
+      <footer
+        ref={ref}
+        data-testid={testId}
+        className={cn(FooterVariants({ bgColor, padding, gap, height }), className)}
+        {...props}
+      >
+        {children}
+      </footer>
+    );
+  }
+);
+
+Footer.displayName = 'Footer';
 
 export default Footer;
