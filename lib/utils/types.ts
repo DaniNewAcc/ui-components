3abbac1069d7b34f087dd3df087c0426de8c4fd7
Daniele <ddaaaniii.98@gmail.com>
2025-07-03 17:01:03 +0200

    feat(utils): introduce helper for ref forwarding in polymorphic components

--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -1,4 +1,10 @@
-import React from "react";
+import React, {
+  ComponentPropsWithoutRef,
+  ElementType,
+  ForwardRefExoticComponent,
+  ReactElement,
+  RefAttributes,
+} from 'react';
 
 // allow to dynamically define the native html element for the component
 type AsProp<C extends React.ElementType> = {
@@ -9,8 +15,7 @@ type AsProp<C extends React.ElementType> = {
 type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
 
 // helper for passing correctly ref to the component
-export type PolymorphicRef<C extends React.ElementType> =
-  React.ComponentPropsWithRef<C>["ref"];
+export type PolymorphicRef<C extends React.ElementType> = React.ComponentPropsWithRef<C>['ref'];
 
 // allow support for html attributes related to the component defined by the AsProp type
 
@@ -26,3 +31,26 @@ export type PolymorphicComponentWithRef<
   C extends React.ElementType,
   Props = object,
 > = PolymorphicComponent<C, Props> & { ref?: PolymorphicRef<C> };
+
+// Helper type to merge your custom props with the props of the `as` element, omitting conflicts
+export type PolymorphicProps<C extends ElementType, Props = {}> = Props & {
+  as?: C;
+} & Omit<ComponentPropsWithoutRef<C>, keyof Props | 'as'>;
+
+// Helper function to allow correct ref forwarding in polymorphic component
+export function forwardRefWithAs<C extends ElementType, Props = {}>(
+  render: (props: PolymorphicProps<C, Props>, ref: PolymorphicRef<C>) => ReactElement | null
+) {
+  type Comp<AsC extends ElementType = C> = PolymorphicProps<AsC, Props> & {
+    ref?: PolymorphicRef<AsC>;
+  };
+
+  type CompType = <AsC extends ElementType = C>(props: Comp<AsC>) => ReactElement | null;
+
+  const Forwarded = React.forwardRef(render as any) as ForwardRefExoticComponent<
+    PolymorphicProps<C, Props> & RefAttributes<PolymorphicRef<C>>
+  > &
+    CompType;
+
+  return Forwarded;
+}
