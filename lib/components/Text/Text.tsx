1c812934701f1a7785efb53385657d6c4549248a
Daniele <ddaaaniii.98@gmail.com>
2025-07-03 21:03:34 +0200

    feat(Text): add ref forwarding support

--- lib/components/Text/Text.tsx
+++ lib/components/Text/Text.tsx
@@ -1,32 +1,42 @@
 import { TextVariants } from '@/utils/variants';
 import { cn } from '@utils/cn';
-import { PolymorphicComponent } from '@utils/types';
+import { forwardRefWithAs, PolymorphicProps, PolymorphicRef } from '@utils/types';
 import { VariantProps } from 'class-variance-authority';
+import { ElementType } from 'react';
 
-type TextProps<C extends React.ElementType> = VariantProps<typeof TextVariants> &
-  PolymorphicComponent<C, { as?: C; testId?: string; level?: 1 | 2 | 3 | 4 | 5 | 6 }>;
-
-const Text = <C extends React.ElementType = 'span'>({
-  as,
-  variant,
-  level,
-  className,
-  children,
-  testId,
-  ...props
-}: TextProps<C>) => {
-  let Tag: React.ElementType = as || 'span';
-
-  if (!as && variant === 'heading') {
-    Tag = level ? `h${level}` : 'h1';
-  }
-
-  return (
-    <Tag data-testid={testId} className={cn(TextVariants({ variant }), className)} {...props}>
-      {children}
-    </Tag>
-  );
+type TextOwnProps = {
+  testId?: string;
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
 };
+
+type TextProps<C extends ElementType> = PolymorphicProps<
+  C,
+  TextOwnProps & VariantProps<typeof TextVariants>
+>;
+
+const Text = forwardRefWithAs(
+  <C extends React.ElementType = 'span'>(
+    { as, variant, level, className, children, testId, ...props }: TextProps<C>,
+    ref: PolymorphicRef<C>
+  ) => {
+    let Tag: React.ElementType = as || 'span';
+
+    if (!as && variant === 'heading') {
+      Tag = level ? `h${level}` : 'h1';
+    }
+
+    return (
+      <Tag
+        data-testid={testId}
+        ref={ref}
+        className={cn(TextVariants({ variant }), className)}
+        {...props}
+      >
+        {children}
+      </Tag>
+    );
+  }
+);
 
 Text.displayName = 'Text';
 
