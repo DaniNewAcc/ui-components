2ab91461df4dca74ecdb3bd04173073bef245bd1
Daniele <ddaaaniii.98@gmail.com>
2025-07-07 14:08:30 +0200

    feat(Loader): add ref forwarding support

--- lib/components/Loader/Loader.tsx
+++ lib/components/Loader/Loader.tsx
@@ -3,9 +3,9 @@ import { FlexVariants, LoaderVariants } from '@/utils/variants';
 import Flex from '@components/Flex';
 import { cn } from '@utils/cn';
 import { VariantProps } from 'class-variance-authority';
-import { ComponentProps, ReactNode } from 'react';
+import { ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react';
 
-type LoaderProps = ComponentProps<'div'> &
+type LoaderProps = ComponentPropsWithoutRef<'div'> &
   VariantProps<typeof LoaderVariants> &
   VariantProps<typeof FlexVariants> & {
     testId?: string;
@@ -13,48 +13,54 @@ type LoaderProps = ComponentProps<'div'> &
     loaderClassName?: string;
   };
 
-const Loader = ({
-  loaderType,
-  size,
-  borderColor,
-  align,
-  gap,
-  justify,
-  testId = 'loader',
-  className,
-  loaderClassName,
-  children,
-  ...props
-}: LoaderProps) => {
-  const loaderClass = LoaderVariants({ loaderType, size, borderColor });
+const Loader = forwardRef<React.ElementRef<'div'>, LoaderProps>(
+  (
+    {
+      loaderType,
+      size,
+      borderColor,
+      align,
+      gap,
+      justify,
+      testId = 'loader',
+      className,
+      loaderClassName,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    const loaderClass = LoaderVariants({ loaderType, size, borderColor });
 
-  const createDots = () =>
-    Array.from({ length: 3 }).map((_, i) => (
-      <span key={i} className={cn(loaderClass, loaderClassName)} />
-    ));
+    const createDots = () =>
+      Array.from({ length: 3 }).map((_, i) => (
+        <span key={i} className={cn(loaderClass, loaderClassName)} />
+      ));
 
-  return (
-    <Flex
-      align={'center'}
-      gap={'sm'}
-      justify={'center'}
-      data-testid={testId}
-      role="status"
-      aria-busy="true"
-      className={cn(FlexVariants({ align, gap, justify }), className)}
-      {...props}
-    >
-      <Flex gap={'sm'}>
-        {loaderType === 'dots' ? (
-          createDots()
-        ) : (
-          <span className={cn(loaderClass, loaderClassName)} />
-        )}
+    return (
+      <Flex
+        ref={ref}
+        align={'center'}
+        gap={'sm'}
+        justify={'center'}
+        data-testid={testId}
+        role="status"
+        aria-busy="true"
+        className={cn(FlexVariants({ align, gap, justify }), className)}
+        {...props}
+      >
+        <Flex gap={'sm'}>
+          {loaderType === 'dots' ? (
+            createDots()
+          ) : (
+            <span className={cn(loaderClass, loaderClassName)} />
+          )}
+        </Flex>
+        {children}
       </Flex>
-      {children}
-    </Flex>
-  );
-};
+    );
+  }
+);
 
 Loader.displayName = 'Loader';
 
