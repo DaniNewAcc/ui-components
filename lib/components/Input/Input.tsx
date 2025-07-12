c5d38bd3172d2e2ba5f7a1ffcada17fc2a0d092b
Daniele <ddaaaniii.98@gmail.com>
2025-07-12 23:22:01 +0200

    feat(Input): Improve accessibility with ARIA attributes aria-describedby and aria-invalid

--- lib/components/Input/Input.tsx
+++ lib/components/Input/Input.tsx
@@ -6,18 +6,26 @@ import React, { ComponentPropsWithoutRef, forwardRef } from 'react';
 type InputProps = ComponentPropsWithoutRef<'input'> &
   VariantProps<typeof InputVariants> & {
     testId?: string;
+    errorId?: string;
+    hintId?: string;
   };
 
 const Input = forwardRef<React.ElementRef<'input'>, InputProps>(
-  ({ error, className, testId = 'input', ...props }, ref) => (
-    <input
-      data-testid={testId}
-      ref={ref}
-      aria-invalid={error ? 'true' : undefined}
-      className={cn(InputVariants({ error }), className)}
-      {...props}
-    />
-  )
+  ({ error, className, errorId, hintId, testId = 'input', ...props }, ref) => {
+    const describedByIds =
+      [props['aria-describedby'], error && errorId, hintId].filter(Boolean).join(' ') || undefined;
+
+    return (
+      <input
+        data-testid={testId}
+        ref={ref}
+        aria-invalid={error ? 'true' : undefined}
+        aria-describedby={describedByIds}
+        className={cn(InputVariants({ error }), className)}
+        {...props}
+      />
+    );
+  }
 );
 
 Input.displayName = 'Input';
