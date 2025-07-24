2a9451dc2bb6d8a9718c90833a9f63de6c750c73
Daniele <ddaaaniii.98@gmail.com>
2025-07-24 15:26:31 +0200

    feat(Overlay): create reusable overlay component

---
+++ lib/components/Overlay/Overlay.tsx
@@ -0,0 +1,45 @@
+import { cn } from '@utils/cn';
+import { ComponentPropsWithoutRef, useCallback } from 'react';
+
+type OverlayProps = ComponentPropsWithoutRef<'div'> & {
+  testId?: string;
+  closeOnClickOutside?: boolean;
+  zIndex?: number;
+  opacity?: number;
+  onClickOutside?: () => void;
+};
+
+const Overlay = ({
+  className,
+  closeOnClickOutside = true,
+  onClickOutside,
+  zIndex = 40,
+  opacity = 0.5,
+  testId = 'overlay',
+  ...props
+}: OverlayProps) => {
+  const handleClick = useCallback(() => {
+    if (closeOnClickOutside && onClickOutside) {
+      onClickOutside();
+    }
+  }, [closeOnClickOutside, onClickOutside]);
+
+  return (
+    <div
+      data-testid={testId}
+      aria-hidden="true"
+      className={cn(
+        'ui:fixed ui:inset-0',
+        `ui:z-[${zIndex}]`,
+        `ui:bg-black ui:opacity-${opacity * 100}`,
+        className
+      )}
+      onClick={handleClick}
+      {...props}
+    />
+  );
+};
+
+Overlay.displayName = 'Overlay';
+
+export default Overlay;
