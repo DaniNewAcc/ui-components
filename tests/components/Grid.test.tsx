b0e7a5617b96d457ad16b2c12aabab3a58433d3c
Daniele <ddaaaniii.98@gmail.com>
2025-08-05 11:33:07 +0200

    test(Grid): add unit tests for height attributes

--- tests/components/Grid.test.tsx
+++ tests/components/Grid.test.tsx
@@ -11,15 +11,12 @@ describe('Grid', () => {
   it('should wrap content in Scrollable with default class when scrollable is true', () => {
     render(<Grid scrollable={true} />);
     const scrollable = screen.getByTestId('grid').parentElement;
-    expect(scrollable?.className).toMatch('ui:max-h-[300px]');
+    expect(scrollable?.className).toMatch('ui:max-h-[50vh]');
   });
 
   it('should wrap content in Scrollable with custom props when ScrollableProps are provided', () => {
     render(
-      <Grid
-        scrollable
-        scrollableProps={{ className: 'custom-scroll', 'data-testid': 'scroll-wrap' }}
-      />
+      <Grid scrollable scrollableProps={{ className: 'custom-scroll', testId: 'scroll-wrap' }} />
     );
     const scrollable = screen.getByTestId('scroll-wrap');
     expect(scrollable).toBeInTheDocument();
@@ -29,6 +26,20 @@ describe('Grid', () => {
   it('should not wrap in Scrollable when scrollable is false', () => {
     render(<Grid scrollable={false} />);
     const grid = screen.getByTestId('grid');
-    expect(grid.parentElement?.className).not.toMatch('ui:max-h-[300px]|custom-scroll');
+    expect(grid.parentElement?.className).not.toMatch('ui:max-h-[50vh]|custom-scroll');
+  });
+
+  it('should render Scrollable with the correct height', () => {
+    render(<Grid scrollable scrollableProps={{ style: { height: '100px' } }} />);
+    const grid = screen.getByTestId('grid');
+    const wrapper = grid.parentElement;
+    expect(wrapper?.style.height).toBe('100px');
+  });
+
+  it('should render Scrollable with the correct maxHeight', () => {
+    render(<Grid scrollable scrollableProps={{ style: { maxHeight: '100px' } }} />);
+    const grid = screen.getByTestId('grid');
+    const wrapper = grid.parentElement;
+    expect(wrapper?.style.maxHeight).toBe('100px');
   });
 });
